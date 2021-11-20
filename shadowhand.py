10bd621262194560492682f737a306e17d4fc9bc
Neil Mehta <neil.mehta310501@gmail.com>
2021-11-20 15:44:03 +0530

    Basic Implementation for creating window glfw

---
+++ shadowhand.py
@@ -0,0 +1,16 @@
+"""ShadowHand Environment Wrappers."""
+import os
+
+import numpy as np
+import gym
+from gym import spaces
+
+env = gym.make("HandManipulateBlock-v0")
+done, observation = False, env.reset()
+
+while True:
+    env.render()
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+    if done:
+        env.reset()
